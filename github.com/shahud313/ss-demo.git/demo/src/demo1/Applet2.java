8ea327a1c7a30cd4c462e57f8a87c9768722c6cb
admin <admin@DESKTOP-G86DG79>
2022-04-06 13:29:04 +0530

    first commit

---
+++ github.com/shahud313/ss-demo.git/demo/src/demo1/Applet2.java
@@ -0,0 +1,14 @@
+package demo1;
+
+import java.awt.Button;
+import java.awt.TextField;
+
+public class Applet2 {
+	Button b;
+	TextField t;
+	public void init() {
+		b= new Button("submit");
+		add(b);
+		t= new TextField();
+		add(t);
+}}
