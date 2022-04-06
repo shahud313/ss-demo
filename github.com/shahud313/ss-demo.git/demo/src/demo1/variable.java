61ae85f6514e4646fd5720f4b948121d861ec65e
admin <admin@DESKTOP-G86DG79>
2022-04-06 23:47:49 +0530

    variable addition

---
+++ github.com/shahud313/ss-demo.git/demo/src/demo1/variable.java
@@ -0,0 +1,13 @@
+package demo1;
+
+public class variable 
+{  
+		public static void main(String[] args){    
+		int a=10;    
+		int b=10;    
+		int c=a+b;    
+		System.out.println(c);    
+		}  
+		}    
+
+
