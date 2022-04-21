d7d359d3451354990eff0e664d466cb356c05abc
admin <admin@DESKTOP-G86DG79>
2022-04-21 23:02:50 +0530

    20-04-2022  Threads ,current Threads Runnable

--- github.com/shahud313/ss-demo.git/demo/src/com/guiapps/ThreadDemo6.java
+++ github.com/shahud313/ss-demo.git/demo/src/com/guiapps/ThreadDemo6.java
@@ -6,12 +6,19 @@ import java.awt.TextField;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-public class ThreadDemo6  extends Frame {
+public class ThreadDemo6  extends Frame implements Runnable {
 TextField t1,t2;
+Thread th1,th2;
+int i,j;
+
 public ThreadDemo6() {
 	setLayout(new FlowLayout());
 	add(t1=new TextField(15));
 	add(t2=new TextField(15));
+	th1= new Thread(this);
+	th2=new Thread(this);
+	th1.start();
+	th2.start();
 	addWindowFocusListener(new WindowAdapter() {
 	@Override
 	
@@ -20,7 +27,7 @@ public ThreadDemo6() {
 		
 	}
 	});
-	setSize(40,400);
+	setSize(300,300);
 	setVisible(true);
 	
 	
@@ -28,5 +35,26 @@ public ThreadDemo6() {
 public static void main(String[] args) {
 	new ThreadDemo6();
 	
+}
+@Override
+public void run() {
+	// TODO Auto-generated method stub
+	while(true) {
+		Thread th = Thread.currentThread();
+		if(th.equals(th1)) {
+			t1.setText(String.valueOf(i));
+			if(i==100)
+				i=0;
+			else i++;
+						
+		}
+		if(th.equals(th2)) {
+			t2.setText(String.valueOf(j));
+			if(j==100)
+				j=0;
+			else j++;
+			
+		}
+	}
 }
 }
