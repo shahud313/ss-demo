96b4bca1af433330265e3e5db4787c688f90c370
admin <admin@DESKTOP-G86DG79>
2022-04-27 23:50:13 +0530

    Exceptional handling  26-04-2022

---
+++ github.com/shahud313/ss-demo.git/demo/src/threadPractice/ExceptionDemo.java
@@ -0,0 +1,13 @@
+package threadPractice;
+
+public class ExceptionDemo {
+public void  m1() throws Exception {
+	
+}
+}
+class Qt {
+	public void m2() throws Exception{
+		ExceptionDemo ed = new ExceptionDemo();
+		ed.m1();
+	}
+}
