c1c51ec96e297eb6c6fc8587502ed1aeed02c927
admin <admin@DESKTOP-G86DG79>
2022-05-15 14:42:30 +0530

    29-04-22

--- github.com/shahud313/ss-demo.git/demo/src/collectionFramework/CollectionFEx1.java
+++ github.com/shahud313/ss-demo.git/demo/src/collectionFramework/CollectionFEx1.java
@@ -1,5 +1,16 @@
 package collectionFramework;
 
-public class CollectionFEx1 {
+import java.util.ArrayList;
 
+public class CollectionFEx1 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<String> animals = new ArrayList<>();
+        // Add elements
+        animals.add("Dog");
+        animals.add("Cat");
+        animals.add("Horse");
+
+        System.out.println("ArrayList: " + animals);
+	}
 }
