8f06c7a82419f2142f0d6e51af3f48b6801b06de
admin <admin@DESKTOP-G86DG79>
2022-05-15 14:46:49 +0530

    1-05-2022

---
+++ github.com/shahud313/ss-demo.git/demo/src/collectionFramework/LinkedListEx.java
@@ -0,0 +1,17 @@
+package collectionFramework;
+
+import java.util.LinkedList;
+
+public class LinkedListEx {
+	public static void main(String[] args){
+
+	    // create linkedlist
+	    LinkedList<String> animals = new LinkedList<>();
+
+	    // Add elements to LinkedList
+	    animals.add("Dog");
+	    animals.add("Cat");
+	    animals.add("Cow");
+	    System.out.println("LinkedList: " + animals);
+	}
+}
