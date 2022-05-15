e6906690a91c8c2bb09dc082ec1be43caa65878b
admin <admin@DESKTOP-G86DG79>
2022-05-15 14:44:57 +0530

    30-04-2022

---
+++ github.com/shahud313/ss-demo.git/demo/src/collectionFramework/CollectionFEx3.java
@@ -0,0 +1,30 @@
+package collectionFramework;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class CollectionFEx3 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 // Creating list using the LinkedList class
+        List<Integer> numbers = new LinkedList<>();
+
+        // Add elements to the list
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        System.out.println("List: " + numbers);
+
+        // Access element from the list
+        int number = numbers.get(2);
+        System.out.println("Accessed Element: " + number);
+
+        // Using the indexOf() method
+        int index = numbers.indexOf(2);
+        System.out.println("Position of 3 is " + index);
+
+        // Remove element from the list
+        int removedNumber = numbers.remove(1);
+        System.out.println("Removed Element: " + removedNumber);
+	}
+}
